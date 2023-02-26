0582220882a1d6e59d33f7da4ca0a877179c4c0f
baudelotphilippe <philippe@baudelot.eu>
2023-02-26 14:15:32 +0100

    change file location

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'json');
+  const jsonDirectory = path.join(process.cwd(), 'events');
   //Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/events.json', 'utf8');
   //Return the content of the data file in json format

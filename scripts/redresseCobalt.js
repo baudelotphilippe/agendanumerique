ecfdf2ee6e8483545f2a87efaa2a6dfa8266884f
baudelotphilippe <philippe@baudelot.eu>
2023-03-07 08:05:21 +0100

    fox startDate

--- scripts/redresseCobalt.js
+++ scripts/redresseCobalt.js
@@ -1,5 +1,5 @@
 const cobalt = (infos) => {
-  console.log("infos", infos)
+  console.log("infos", infos);
   //remove last virgule
   const lastVirgule = infos.lastIndexOf(",");
   const infos2 = infos.substring(0, lastVirgule) + "}";
@@ -7,26 +7,35 @@ const cobalt = (infos) => {
   const info3 = infos2.replace(/(\r\n|\n|\r|\t)/gm, "");
   // console.log("infos", info3)
 
-// converti en array pour chercher les clés pourries
+  // converti en array pour chercher les clés pourries, mis les espaces pour éviter de récupérer du texte avec des , ...
   const infosAsArr = info3.split(",  ");
   for (var j = 0; j < infosAsArr.length; j++) {
     // enlève les " en trop"
     if (infosAsArr[j].match('"name":')) {
-      // console.log(infosAsArr[j]) ;
       const value = infosAsArr[j].substring(8);
       const info4 = value.replace(/\"/g, "");
       infosAsArr[j] = `"name": "${info4}"`;
-      // console.log(infosAsArr) ;
     }
 
     if (infosAsArr[j].match('"description":')) {
-      // console.log("in", infosAsArr[j]);
       const value = infosAsArr[j].substring(14, infosAsArr[j].length - 2);
       const info4 = value.replace(/\"/g, "");
       infosAsArr[j] = `"description": "${info4}"}`;
-      // console.log("out", infosAsArr);
+    }
+    if (infosAsArr[j].match('"startDate":')) {
+      const value = infosAsArr[j].substring(12);
+      // console.log(value);
+      const arrDate = value.split("T");
+      const arrHeure = arrDate[1].split("-");
+      //remove first content (c'est un doublon...)
+      arrHeure.shift();
+      const newArrHeure=arrHeure.join('-')
+     const newStartDate=`${arrDate[0].toString()}T${newArrHeure}`
+    //  console.log("newStartDate",newStartDate);
+      infosAsArr[j] = `"startDate": ${newStartDate}`;
     }
   }
+  console.log(infosAsArr)
   const arrEnString = infosAsArr.toString();
   return JSON.parse(arrEnString);
 };

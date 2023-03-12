6c302031769b02a98665efa7d730852ec93fca26
baudelotphilippe <philippe@baudelot.eu>
2023-03-12 20:31:08 +0100

    refacto cobalt

--- scripts/compilCobalt.js
+++ scripts/compilCobalt.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const cobalt = require("./redresseCobalt");
+const saveFile = require("./utils/file");
 
 async function getStaticProps() {
   const { data } = await axios.get(
@@ -9,34 +10,15 @@ async function getStaticProps() {
   );
   const $ = cheerio.load(data);
   $('.evo_event_schema').each((index, item) => { 
-    const urlEvent=item.children[0].attribs.href;
 
-   // recup le jsonLD
     const event = cobalt(item.children[item.children.length-1].children[0].data);
-    const filterContent = {
-      name: event.name ?? "",
-      description: event.description ?? "",
-      startDate: event.startDate ?? "",
-      endDate: event.endDate ?? "",
-      location: {
-        name: event.location?.name ?? "Cobalt Poitiers",
-        address: {
-          addressLocality: event.location?.address?.addressLocality ?? "86000 Poitiers",
-          streetAddress: event.location?.address?.streetAddress ?? "5 Rue Victor Hugo",
-        },
-      },
-      image: event.image ?? "",
-      organizer: event.organizer?.name ?? "",
-      url:urlEvent
-    };
-    const newName = event.name.replace(/ |:/g, "-");
-    const newName2=newName.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    fs.writeFileSync(
-      `./events/cobalt/${encodeURIComponent(newName2)}.json`,
-      `<script type="application/ld+json">${JSON.stringify(
-        filterContent
-      )}</script>`
-    );
+
+    event.url=item.children[0].attribs.href;
+    event.location.name="Cobalt Poitiers"
+    event.location.address.addressLocality="86000 Poitiers"
+    event.location.address.streetAddress="5 Rue Victor Hugo"
+
+    saveFile("cobalt", event)
   });
 }
 
--- scripts/redresseCobalt.js
+++ scripts/redresseCobalt.js
@@ -1,52 +1,34 @@
+const { type } = require("os");
+const { stringify } = require("querystring");
+const emptyEvent = require("./utils/emptyEvent");
+
 const cobalt = (infos) => {
   // console.log("infos", infos);
   //remove last virgule
   let cleanInfos = infos.substring(0, infos.lastIndexOf(",")) + "}";
   //remove tab & CR
   cleanInfos = cleanInfos.replace(/(\r\n|\n|\r|\t)/gm, "");
-
-  // converti en array pour chercher les clés pourries, mis les espaces pour éviter de récupérer du texte avec des , ...
-  const infosAsArr = cleanInfos.split(",  ");
-  for (var j = 0; j < infosAsArr.length; j++) {
-    // enlève les " en trop"
-    if (infosAsArr[j].match('"name":')) {
-      const value = infosAsArr[j].substring(8);
-      const cleanValue = value.replace(/\"/g, "");
-      infosAsArr[j] = `"name": "${cleanValue}"`;
-    }
-
-    if (infosAsArr[j].match('"description":')) {
-      const value = infosAsArr[j].substring(14, infosAsArr[j].length - 2);
-      const cleanValue = value.replace(/\"/g, "");
-      infosAsArr[j] = `"description": "${cleanValue}"}`;
-    }
-    if (infosAsArr[j].match('"startDate":')) {
-      const value = infosAsArr[j].substring(12);
-      const newStartDate=reformatDate(value)
-      infosAsArr[j] = `"startDate": ${newStartDate}`;
-    }
-    if (infosAsArr[j].match('"endDate":')) {
-      const value = infosAsArr[j].substring(10);
-      const newEndDate=reformatDate(value)
-      infosAsArr[j] = `"endDate": ${newEndDate}`;
-    }
-  }
-  console.log(infosAsArr)
-  const arrEnString = infosAsArr.toString();
-  return JSON.parse(arrEnString);
+  cleanInfos = JSON.parse(cleanInfos);
+  emptyEvent.name = cleanInfos.name;
+  emptyEvent.description = cleanInfos.description;
+  emptyEvent.image = cleanInfos.image;
+  emptyEvent.startDate = reformatDate(cleanInfos.startDate);
+  emptyEvent.endDate = reformatDate(cleanInfos.endDate);
+  return emptyEvent;
 };
 
 const reformatDate = (date) => {
   const arrDateHeure = date.split("T");
 
-  const arrDate=arrDateHeure[0].split("-");
-  arrDate[1]=("0"+arrDate[1]).slice(-2)
-  
+  const arrDate = arrDateHeure[0].split("-");
+  //force sur 2 chars
+  arrDate[1] = ("0" + arrDate[1]).slice(-2);
+
   const arrHeure = arrDateHeure[1].split("-");
   //remove first content (c'est un doublon...)
   arrHeure.shift();
-  const newArrHeure=arrHeure.join('-')
- return `${arrDate[0]}-${arrDate[1]}-${arrDate[2]}T${newArrHeure}`
-}
+  const newArrHeure = arrHeure.join("-");
+  return `${arrDate[0]}-${arrDate[1]}-${arrDate[2]}T${newArrHeure}`;
+};
 
 module.exports = cobalt;

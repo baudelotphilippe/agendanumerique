f5d8b692d32b683f8f6fbfebb39f6d3b8524fac3
baudelotphilippe <philippe@baudelot.eu>
2023-03-15 16:36:08 +0100

    clean

--- pages/api/staticEvent.js
+++ pages/api/staticEvent.js
@@ -1,9 +1,8 @@
-import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'json');
+  // const jsonDirectory = path.join(process.cwd(), 'json');
   //Read the json data file data.json
   const query = req.query;
   const {slug, categorie}=query

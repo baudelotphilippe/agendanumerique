519fa9892132a1228ffbc3596ccca481f40d5a7f
baudelotphilippe <philippe@baudelot.eu>
2023-12-09 14:06:01 +0100

    ajoute description #SEO

--- components/header.js
+++ components/header.js
@@ -3,8 +3,8 @@ import Head from "next/head";
 const Header = ({ title, datas }) => {
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="description" content="" />
+      <title>{title} - Agenda numérique Poitiers</title>
+      <meta name="description" content="Agenda du numéroque sur Poitiers" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <script type="application/ld+json">{datas}</script>

bd842f5757e5e4fe9eba4fc144eb5b41576ef21f
baudelotphilippe <philippe@baudelot.eu>
2024-01-13 09:24:40 +0100

    remove axios

--- scripts/compilPwn.js
+++ scripts/compilPwn.js
@@ -1,22 +1,23 @@
-import axios from "axios";
-import redressePwn from "./redressePwn.js"
+import redressePwn from "./redressePwn.js";
 import * as cheerio from "cheerio";
-import {saveFile, cleanFolder} from "./utils/file.js";
+import { saveFile, cleanFolder } from "./utils/file.js";
 
-const infosFilename={workingFolder:"pwn", i:false, uniqueId:false}
+const infosFilename = { workingFolder: "pwn", i: false, uniqueId: false };
 
 async function loadEvent(urlEvent) {
-  const { data } = await axios.get(`https://pwn-association.org${urlEvent}`);
+  const data = await fetch(`https://pwn-association.org${urlEvent}`).then(
+    (res) => res.text()
+  );
   const event = redressePwn(data);
   saveFile(infosFilename, event);
 }
 
 async function compilPwn() {
-  const { data } = await axios.get(
+  const data = await fetch(
     "https://pwn-association.org/tous-les-evenements-pwn/"
-  );
+  ).then((res) => res.text());
   const $ = cheerio.load(data);
-  await cleanFolder(infosFilename.workingFolder)
+  await cleanFolder(infosFilename.workingFolder);
   // get all urls from page events
   $(".upcoming-events .event-inner>a").each((index, item) => {
     loadEvent(item.attribs.href);

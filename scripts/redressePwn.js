3e6b2a61afe052ce5ce7bd63ca1760e808309997
baudelotphilippe <philippe@baudelot.eu>
2024-09-16 20:14:00 +0200

    fix pwn

--- scripts/redressePwn.js
+++ scripts/redressePwn.js
@@ -1,64 +1,76 @@
 import * as cheerio from "cheerio";
-import {emptyEvent} from "./utils/emptyEvent.js";
-import {moisEnChiffre} from "./utils/convertDates.js";
+import { emptyEvent } from "./utils/emptyEvent.js";
+import { moisEnChiffre } from "./utils/convertDates.js";
 
-const convertDatePwn=(datePwn) => {
-  const jourHeureDebut=datePwn.split("à")
-  const arrJourDebut=jourHeureDebut[0].trim().split(" ")
+const convertDatePwn = (datePwn) => {
+  const jourHeureDebut = datePwn.split("à");
+  const arrJourDebut = jourHeureDebut[0].trim().split(" ");
   // retire , et force sur 2 digits
-  const formatDay=("0"+arrJourDebut[1].slice(0,-1)).slice(-2)
-   return `${arrJourDebut[2]}-${moisEnChiffre(arrJourDebut[0])}-${formatDay}T${jourHeureDebut[1].trim()}:00`
-}
+  const formatDay = ("0" + arrJourDebut[1].slice(0, -1)).slice(-2);
+  return `${arrJourDebut[2]}-${moisEnChiffre(
+    arrJourDebut[0]
+  )}-${formatDay}T${jourHeureDebut[1].trim()}:00`;
+};
 
 export default function redressePwn(data) {
   const $ = cheerio.load(data);
-  let name=$('.event-title')[0].children[0].data
+  let name = $(".event-title")[0].children[0].data;
   const event = {
-    ...emptyEvent
+    ...emptyEvent,
   };
-  // trim enlève char avant et après string  
+  // trim enlève char avant et après string
   event.name = name.replace(/(\r\n|\n|\r|\t)/gm, "").trim();
 
-  event.url=$("meta[property='og:url']").attr("content");
-  event.image=$("meta[property='og:image']").attr("content");
+  event.url = $("meta[property='og:url']").attr("content");
+  event.image = $("meta[property='og:image']").attr("content");
 
-  let description=$('.event-description')[0].children//.length//[4]
+  let description = $(".event-description")[0].children; //.length//[4]
 
-  let beginRecord=false
-  let contentDescription="";
+  let beginRecord = false;
+  let contentDescription = "";
   description.forEach((elem, index) => {
     // console.log("index >", index)
-    
+
     if (elem.children) {
+      if (elem.children[0].data == "infos pratiques") {
+        const dateElement = $("div.event-description li").filter((i, el) => {
+          return $(el).text().includes("Date et heure");
+        });
+        const laDate = dateElement
+          .text()
+          .trim()
+          .replace(/\s+/g, " ")
+          .replace("Date et heure :", "")
+          .trim();
 
-      if (elem.children[0].data=="infos pratiques") {
-        // console.log("indexw ==>", description[index+1].next.children[1].children[2].children[0].data)//adresse
+        event.startDate = convertDatePwn(laDate);
+        event.endDate = event.startDate;
+        const locationElement = $("div.event-description li").filter(
+          (i, el) => {
+            return $(el).text().includes("Lieu :");
+          }
+        );
 
-        const lesDates=description[index+1].next.children[2].next.children[1].data
-        const cleanDates = lesDates.replace(/(\r\n|\n|\r|\t)/gm, "").trim();
-        const arrDates=cleanDates.split("–")
-        
-        event.startDate=convertDatePwn(arrDates[0])
-        event.endDate=convertDatePwn(arrDates[1])
-
-        event.location.name=description[index+1].next.children[1].children[2].children[0].data.trim()
+        const location = locationElement.find("a").text().trim();
+        event.location.name = location;
       }
 
-      if (elem.children[0].data=="description de l'intervenant") {
-        beginRecord=false
+      if (elem.children[0].data == "description de l'intervenant") {
+        beginRecord = false;
       }
       if (beginRecord) {
-        elem.children.forEach((subElem) =>{
-          if (subElem.data) {contentDescription+=subElem.data}
-        })
+        elem.children.forEach((subElem) => {
+          if (subElem.data) {
+            contentDescription += subElem.data;
+          }
+        });
       }
-      if (elem.children[0].data=="description de l'intervention") {
-        beginRecord=true
+      if (elem.children[0].data == "description de l'intervention") {
+        beginRecord = true;
       }
     }
-  }
-  )
-  event.description=contentDescription
-  event.organizer="Pwn"
-  return event
-};
+  });
+  event.description = contentDescription;
+  event.organizer = "Pwn";
+  return event;
+}

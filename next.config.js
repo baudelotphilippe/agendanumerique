d5cdbad92e5bf1be032fd55d85d63a249d3ee57d
baudelotphilippe <philippe@baudelot.eu>
2024-11-27 16:39:59 +0100

    update meetup imgs

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'secure-content.meetupstatic.com'
+      },     
+      {
+        protocol: 'https',
+        hostname: 'secure.meetupstatic.com'
       }
     ],
   },
